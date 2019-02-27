ded56ff0d8690590abe1d7b13cca5c61008d184c
kerrykdiehl@gmail.com <kerrydiehl@Kerrys-MBP.teamnorthwoods.com>
2019-02-27 14:54:04 -0500

    Fix formatting

--- src/TemplateDesigner.js
+++ src/TemplateDesigner.js
@@ -1,80 +1,89 @@
-import React, { Component } from 'react';
-import './tailwind.css';
+import React, { Component } from "react";
+import "./tailwind.css";
 
-const FormTemplate = (props) => {
+const FormTemplate = props => {
   return (
     <div className="w-3/4">
       <h1>Template</h1>
-      {props.fields.map((field,index) => {
-        let selected = props.selectedFieldId === field.id
+      {props.fields.map((field, index) => {
+        let selected = props.selectedFieldId === field.id;
         let selectedStyle = {
-          padding : "10px",
-          backgroundColor : "purple"
-        }
+          padding: "10px",
+          backgroundColor: "purple"
+        };
         return (
-          <div style={selected ? selectedStyle : {}} key={field.id}
-          onClick={
-            () => {
-              props.onSelectField(field.id)
-            }
-          }>
-            {field.type === "textarea" ? 
-              <textarea/> :
-              <input type={field.type}/>}
+          <div
+            style={selected ? selectedStyle : {}}
+            key={field.id}
+            onClick={() => {
+              props.onSelectField(field.id);
+            }}
+          >
+            {field.type === "textarea" ? (
+              <textarea />
+            ) : (
+              <input type={field.type} />
+            )}
           </div>
-        )
+        );
       })}
-     
     </div>
-  )
-}
+  );
+};
 
-const ControlType = (props) => {
+const ControlType = props => {
   return (
-      <button className="bg-blue text-white p-4" onClick={
-        (e) => {
-          props.onAddField(props.type)
-        }
-      }>{props.type}</button>
-  )
-}
+    <button
+      className="bg-blue text-white p-4"
+      onClick={e => {
+        props.onAddField(props.type);
+      }}
+    >
+      {props.type}
+    </button>
+  );
+};
 
-const FormControls = (props) => {
+const FormControls = props => {
   return (
     <div className="w-1/4">
       <h1>Controls</h1>
-      <ControlType type="textfield" {...props}/>
-      <ControlType type="textarea" {...props}/>
-      <ControlType type="date" {...props}/>
-      <ControlType type="checkbox" {...props}/>
+      <ControlType type="textfield" {...props} />
+      <ControlType type="textarea" {...props} />
+      <ControlType type="date" {...props} />
+      <ControlType type="checkbox" {...props} />
     </div>
-  )
-}
+  );
+};
 
 class TemplateDesigner extends Component {
   constructor(props) {
     super(props);
     this.state = {
       fields: [],
-      selectedFieldId : -1
-    }
+      selectedFieldId: -1
+    };
   }
-  addField = (field) => {
+  addField = field => {
     this.setState({
-      fields: [...this.state.fields, {id: new Date().valueOf(),type: field}]
-    })
-  }
+      fields: [...this.state.fields, { id: new Date().valueOf(), type: field }]
+    });
+  };
 
-  selectField = (id) => {
-    this.setState({selectedFieldId : id})
-  }
+  selectField = id => {
+    this.setState({ selectedFieldId: id });
+  };
 
   render() {
     return (
       <div className="container mx-auto">
         <div className="flex bg-grey-lighter">
-        <FormTemplate fields={this.state.fields} onSelectField={this.selectField} selectedFieldId={this.state.selectedFieldId}/>
-        <FormControls onAddField={this.addField} />
+          <FormTemplate
+            fields={this.state.fields}
+            onSelectField={this.selectField}
+            selectedFieldId={this.state.selectedFieldId}
+          />
+          <FormControls onAddField={this.addField} />
         </div>
       </div>
     );
