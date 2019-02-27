d3b2e35d7d0d06490a8ba8bd7554a6c4dff5c6a2
David Mosher <davidmosher@gmail.com>
2019-02-27 11:00:53 -0500

    layout primitives

--- src/TemplateDesigner.js
+++ src/TemplateDesigner.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import './tailwind.css';
+
+const FormTemplate = (props) => {
+  return (
+    <div className="w-3/4">Template</div>
+  )
+}
+
+const FormControls = (props) => {
+  return (
+    <div className="w-1/4">Controls</div>
+  )
+}
+
 
 class TemplateDesigner extends Component {
   render() {
     return (
-      <h1>Hi</h1>
+      <div className="container mx-auto">
+        <div className="flex bg-grey-lighter">
+        <FormTemplate />
+        <FormControls />
+        </div>
+      </div>
     );
   }
 }
