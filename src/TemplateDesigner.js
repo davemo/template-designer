48a1f19ca5394f6304912eae1b214c74006d4a2d
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2019-02-27 12:58:14 -0500

    starting to add the ability to add controls to the form template

--- src/TemplateDesigner.js
+++ src/TemplateDesigner.js
@@ -9,18 +9,33 @@ const FormTemplate = (props) => {
 
 const FormControls = (props) => {
   return (
-    <div className="w-1/4">Controls</div>
+    <div className="w-1/4">
+      <h1>Controls</h1>
+      <button className="bg-blue text-white p-4" onClick={
+        (e) => {
+          debugger;
+          props.fields.push(1);
+        }
+      }>Text Field</button>
+    </div>
   )
 }
 
 
 class TemplateDesigner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: []
+    }
+  }
+
   render() {
     return (
       <div className="container mx-auto">
         <div className="flex bg-grey-lighter">
         <FormTemplate />
-        <FormControls />
+        <FormControls fields={this.state.fields} />
         </div>
       </div>
     );
