306d37bc41906aaa4e71fde76a7cb6952a1ea106
David Mosher <davidmosher@gmail.com>
2019-02-27 10:49:50 -0500

    initial stuff

---
+++ src/TemplateDesigner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TemplateDesigner extends Component {
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+}
+
+export default TemplateDesigner;
